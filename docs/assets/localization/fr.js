fba825c99b4c47396f2b82e1cf53f1e3ec37194f
Valentin Deville <contact@valentin-deville.eu>
2019-02-10 16:50:22 +0100

    Build 10/02/2019

---
+++ docs/assets/localization/fr.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){localize({"header form input&placeholder":"Rechercher...",".paginator a:nth-of-type(1)&html":'<i class="fa fa-arrow-left" aria-hidden="true"></i> Page précédente',".paginator a:nth-of-type(2)&html":'<i class="fa fa-arrow-right" aria-hidden="true"></i> Page suivante'})});
